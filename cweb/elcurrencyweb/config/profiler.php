05ee789c709d209d9b820ee6f0736f5d11e9c036
Герхард PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2023-01-27 10:38:45 -0400

    set profiling class and debugger web system for us, using console lib

--- cweb/elcurrencyweb/config/profiler.php
+++ cweb/elcurrencyweb/config/profiler.php
@@ -1,6 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------------
 | Profiler Sections
@@ -9,6 +7,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | data are displayed when the Profiler is enabled.
 | Please see the user guide for info:
 |
-|	https://codeigniter.com/userguide3/general/profiling.html
+|	http://codeigniter.com/user_guide/general/profiling.html
 |
 */
+
+// Choose which sections you want to show up in your profiler bar.
+
+$config['benchmarks']           = TRUE;
+$config['config']               = TRUE;
+$config['console']               = TRUE;
+$config['controller_info']      = TRUE;
+$config['get']                  = TRUE;
+$config['http_headers']         = TRUE;
+$config['memory_usage']         = TRUE;
+$config['post']                 = TRUE;
+$config['queries']              = TRUE;
+$config['eloquent']             = FALSE;
+$config['uri_string']           = TRUE;
+$config['view_data']            = TRUE;
+$config['query_toggle_count']   = 50;
