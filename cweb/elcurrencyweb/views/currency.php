a2bff47cb50c72d98f4f0243356f00ece0678b86
radioactive99 <ag5193407@gmail.com>
2023-02-03 19:45:30 +0100

    added fetch and modal

--- cweb/elcurrencyweb/views/currency.php
+++ cweb/elcurrencyweb/views/currency.php
@@ -1,144 +1,189 @@
-<div class="col py-3">
-                <!-- <div class="container-pagination d-flex justify-content-between">
-                    <nav aria-label="Page navigation example">
-                        <ul class="pagination">
-                          <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Previous">
-                              <span aria-hidden="true">&laquo;</span>
-                            </a>
-                          </li>
-                          <li class="page-item"><a class="page-link" href="#">1</a></li>
-                          <li class="page-item"><a class="page-link" href="#">2</a></li>
-                          <li class="page-item"><a class="page-link" href="#">3</a></li>
-                          <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next">
-                              <span aria-hidden="true">&raquo;</span>
-                            </a>
-                          </li>
-                        </ul>
-                      </nav>
-
-
-
-                      <form>
-                        <div class="form-group">
-                          <label for="exampleFormControlInput1">Seek</label>
-                          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="seek">
-                        </div>
-                      </form>
-                </div> -->
-                <section>
-                  <div id="liveAlertPlaceholder"></div>
-                  <br>
-                  <div class="d-flex w-100">
-                    
-                    <form class="d-flex m-auto ">
-                      <div class="form-group">
-                        <!-- <label for="seek">Email address</label> -->
-                        <input type="text" class="form-control" id="input-seek" placeholder="VES">
-                        <small id="coin" class="form-text text-muted">Introduzca </small>
-                      </div>
-                      <button type="submit" id="seek" class="btn btn-outline-success ms-2">Solucitar</button>
-                    </form>
-                  </div>
-                  <!-- <h1 class="text-center" id="search">TEXT</h1> -->
-                </section>
-          
-
-                <script>
-
-                </script>
-                <br>
-                <div class="contain-table">
-                  <?php 
-					echo '<h1 style="text-align: center;">Currenct Stored currency for today</h1>';
-						if(is_array($currency_list_dbarraynow))
-						{
-							if(count($currency_list_dbarraynow))
-							{
-								$this->table->clear();
-								$this->table->set_heading(array_keys($currency_list_dbarraynow[0]));
-								echo $this->table->generate($currency_list_dbarraynow);
-							}
-							else
-								echo "There's not data stored today, check if your system already call the api in backend, or check if your DB is configured.";
-						}
-						echo br(2);
-					echo '<h1 style="text-align: center;">Current currency rate for today</h1>';
-						$compare = true;
-						if(is_array($currency_list_apiarray) AND $compare)
-						{
-							if(count($currency_list_apiarray))
-							{
-								$this->table->clear();
-								$this->table->set_template( array( 'table_open' => '<table id="table_id2">',) );
-								$this->table->set_heading(array_keys($currency_list_apiarray[0]));
-								echo $this->table->generate($currency_list_apiarray);
-							}
-							else
-								echo "There's not data received from API lib, please check your internet connection or if you already setup the API exchange call in the system installation";
-						}
-                  ?>
-                </div>
-                <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script> 
-
-                <script>
-                    $(document).ready( function () {
-                          $('#table_id2').DataTable();
-                    } );
-                  // let data = <?php //echo json_encode($currency_list_apiarray); ?>;
-                  // let input = document.getElementById("input-seek");
-                  // let answer =""
-                  // let seek = document.getElementById("seek");
-                  // if(seek ){
-                  //   seek.addEventListener('click', () => {
-                  //     if(answer.hasOwnProperty('MONEDA')){
-                  //       alert(answer, 'success')
-                  //     }
-                  //    })
-                  //   seek.addEventListener('click', function(e){
-                  //     e.preventDefault()
-                  //     if(input.value.trim().length > 0){
-                  //       answer = data.find(tasa => tasa.MON_TASA_MONEDA === input.value.toUpperCase())
-                  //     }
-                  //   },true ) ;
-                  // }
-                  // const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-                  // const alert = (message, type) => {
-                  //   const wrapper = document.createElement('div')
-                  //   wrapper.innerHTML = [
-                  //     `<div class="alert alert-${type} alert-dismissible text-center" role="alert">`,
-                  //     `   <div>${message.MONEDA}</div>`,
-                  //     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-                  //     '</div>'
-                  //   ].join('')
-                    
-                  //   alertPlaceholder.append(wrapper)
-                  // }
-                </script> 
-                  
-                    <!-- <br>
-                  <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-center">
-                      <li class="page-item">
-                        <a class="page-link" href="#" aria-label="Previous">
-                          <span aria-hidden="true">&laquo;</span>
-                        </a>
-                      </li>
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
-                      <li class="page-item">
-                        <a class="page-link" href="#" aria-label="Next">
-                          <span aria-hidden="true">&raquo;</span>
-                        </a>
-                      </li>
-                    </ul>
-                  </nav> -->
-                <!-- colocar contenido -->
+<div class="col py-3" >
+  <div id="contain-modal"></div>
+        <section>
+          <div id="liveAlertPlaceholder"></div>
+          <br>
+        </section>
+        <br>
+        <div class="contain-table">
+          <h1 style="text-align: center;">History of your coins rate</h1>
+          <?php 
+            echo '<h1 style="text-align: center;">Currenct Stored currency for today</h1>';
+              if(is_array($currency_list_dbarraynow))
+              {
+                if(count($currency_list_dbarraynow))
+                {
+                  $this->table->clear();
+                  $this->table->set_template( array( 'table_open' => '<table id="table_id">',) );
+                  $this->table->set_heading(array_keys($currency_list_dbarraynow[0]));
+                  echo $this->table->generate($currency_list_dbarraynow);
+                }
+                else
+                  echo "There's not data stored today, check if your system already call the api in backend, or check if your DB is configured.";
+              }
+              echo br(2);
+          ?>
         </div>
+</div>
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script> 
+    <script>
+       let baseUrl = "<?php echo base_url(); ?>";
+      $(document).ready( function () {
+      
+        // ---------------------------------------------------------------------------
+      
+        table1 = $('#table_id').DataTable(
+          {
+            "order": [0]
+          }
+        );
+        $('#table_id tbody').on('click', 'tr', function () {
+          var data = table1.row(this).data()
+          alert(data,'primary')
 
-            
+          let buttonEditData = document.getElementById("edit");
+
+          buttonEditData.addEventListener("click",function(e){
+            e.preventDefault()
+             let codTasa = document.getElementById("cod_tasa");
+             let monTasaMoneda = document.getElementById("mon_tasa_moneda");
+             let object = {
+              cod_tasa:codTasa.value,
+              mon_tasa_moneda:monTasaMoneda.value
+             }
+            fetch(baseUrl+'index.php/Currency_Manager/updatecurrency',{ 
+              method: 'POST', 
+              object
+            })
+            .then(data=>{
+              console.log(data)
+            })            
+             .catch(error => console.log(error))
+             .finally()
+            })
+
+          
+        });
+
+        
+        // ---------------------------------------------------------------------------
+      
+         const containModal = document.getElementById('contain-modal')
+         let divModal = document.createElement('div')         
+         divModal.innerHTML = [
+           ' <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">',
+           '   <div class="modal-dialog">',
+           '     <div class="modal-content">',
+           '       <div class="modal-header">',
+           '         <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>',
+           '         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>',
+           '       </div>',
+           '       <div class="modal-body">',     
+           '          <form>',
+           '           <div class="input-group  mb-3">',
+           '                <select class="form-select" id="append-button-single-field" data-placeholder="Choose one thing">',
+           '                    <option>Reactive</option>',
+           '                    <option>Solution</option>',
+           '                    <option>Conglomeration</option>',
+           '                    <option>Algoritm</option>',
+           '                    <option>Holistic</option>',
+           '                    <option>Holistic</option>',
+           '                    <option>Solution</option>',
+           '                    <option>Conglomeration</option>',
+           '                    <option>Algoritm</option>',
+           '                    <option>Holistic</option>',
+           '                    <option>Holistic</option>',
+           '                    <option>Holistic</option>',
+           '                    <option>Holistic</option>',
+           '                </select>',
+           '           </div>',
+           '          </form>',
+           '          <small id="count" class="form-text text-muted"></small>',
+
+           '       </div>',
+           '        <div class="modal-footer">',
+           '          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>',
+           '          <button type="button" class="btn btn-primary"  id="button">Understood</button>',
+           '        </div>',
+           '      </div>',
+           '    </div>',
+           '  </div>'
+         ].join('')
+        
+         containModal.append(divModal)
+         $('#staticBackdrop').modal('show');
+
+
+         let buttonGetData = document.getElementById("button");
+     //   console.log(buttonGetData)
+       // buttonGetData.addEventListener(function(){
+          // fetch(baseUrl+'Currency_Manager/savecurrency',{ 
+          //   method: 'POST', 
+          //   body
+          // })
+          // .then()
+          // .catch()
+          // .finally()
+       // })
+
+        //  watch()
+
+      })
+    
+      // ---------------------------------------------------------------------------
+    
+       const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+       const alert = (message, type) => {
+       const wrapper = document.createElement('div')
+         wrapper.innerHTML = [
+           `<div class="alert alert-${type} alert-dismissible text-center custom-alerts" role="alert" style="   position: fixed; width: 55%; left: 30%; top: 30%; z-index: 10000000000;">`,
+           '<br>',
+           `<form  id="edit-form">`, 
+             '<div class="form-group">',
+               '<label for="cod_tasa">cod_tasa</label>',
+               '<input name="cod_tasa" type="number" class="form-control" id="cod_tasa" placeholder='+ message[2] +' >',
+             '</div>',
+             '<div class="form-group">',
+               '<label for="mon_tasa_moneda">mon_tasa_moneda</label>',
+               '<input name="mon_tasa_moneda" type="text" class="form-control" id="mon_tasa_moneda" placeholder='+ message[1] +' >',
+             '</div>',
+             '<br>',
+             '<button type="submit" class="btn btn-outline-success" id="edit">Enviar</button>',
+           '</form>',
+           '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+           '</div>'
+         ].join('')
+         alertPlaceholder.append(wrapper)
+        }
+
+        //uttonEditData.addEventListener(function(){
+          // fetch(baseUrl+'Currency_Manager/savecurrency',{ 
+          //   method: 'POST', 
+          //   body
+          // })
+          // .then()
+          // .catch()
+          // .finally()
+        //})
+
+      //  function watch(){
+      //   let n = 0;
+      //    window.setInterval(function(){
+      //        l.innerHTML = n+' S';
+      //      n++;
+      //      if(n>3){
+      //          return window.clearInterval()
+      //      }
+      //      console.log(n)
+
+      //    },1000);
+
+
+      //  }
+
+ 
+
+    </script>
       </div>
     </div>  
       <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
