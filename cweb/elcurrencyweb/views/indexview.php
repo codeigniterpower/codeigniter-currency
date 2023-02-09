fe869d334639d0e7bfaeec68872ff557095c79f1
Герхард PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2023-02-09 15:40:16 -0400

    put a default index view with links

--- cweb/elcurrencyweb/views/indexview.php
+++ cweb/elcurrencyweb/views/indexview.php
@@ -2,6 +2,42 @@
 
 	$userurl = $this->input->get_post('userurl');
 	echo $userurl;
-	
 	?>
-    
+
+<!-- PROMOTION CURRENCYMANAGER BLOCK-->
+	<section id="content-4col" class="dark-bg bg-color1">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3"> <img src="<?php echo base_url();?>/elcurrencyfiles/media/post-img-3.jpg" alt="" class="screen">
+					<div class="editContent">
+						<h3>Currency Manager</h3>
+						<p class="desc-text">A simple web based currency rate manager for your own currency rate history.</p>
+					</div>
+					<a class="btn btn-default btn-outline-success" href="<?php echo site_url();?>/Login"><i class="icon icon-screen-desktop"></i>Login!</a>
+				</div>
+				<div class="col-md-3"> 
+					<img src="<?php echo base_url();?>/elcurrencyfiles/media/post-img-1.jpg" alt="" class="screen">
+					<div class="editContent">
+						<h3>Open source</h3>
+						<p class="desc-text">Dowdload and setup this, help us or join at gitlab..</p>
+					</div>
+					<a class="btn btn-default btn-outline-success" href="https://gitlab.com/codeigniterpower/codeigniter-currencylib/tree/main/docs"><i class="icon icon-screen-desktop"></i>Sources!</a>
+				</div>
+				<div class="col-md-3"> <img src="<?php echo base_url();?>/elcurrencyfiles/media/post-img-2.jpg" alt="" class="screen">
+					<div class="editContent">
+						<h3>Exchangerate integrated</h3>
+						<p class="desc-text">No fluff, just setup your API key We stored into DB if you dont have paid key.</p>
+					</div>
+					<a class="btn btn-default btn-outline-success" href="https://apilayer.com/marketplace/exchangerates_data-api#pricing"><i class="icon icon-screen-desktop"></i> Try Now!</a>
+				</div>
+				<div class="col-md-3"> <img src="<?php echo base_url();?>/elcurrencyfiles/media/post-img-4.jpg" alt="" class="screen">
+					<div class="editContent">
+						<h3>Documentation</h3>
+						<p class="desc-text">The detailed documentation will help you in adjusting the project.</p>
+					</div>
+					<a class="btn btn-default btn-outline-success" href="../../docs"><i class="icon icon-screen-desktop"></i>Read it!</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	
