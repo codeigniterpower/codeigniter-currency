1e8d7925241a9974706e78a4d84f99b9e3daab0e
mckaygerhard <mckaygerhard@gmail.com>
2023-05-11 15:26:27 -0400

    database - preliminary support for sqlite
    * currently sqlite is not supported cos we have a SUBSTRING function
      that we need to change
    * related to https://gitlab.com/codeigniterpower/codeigniter-currencylib/-/issues/15
    * related to commit 3cd0a59552903658a93c4fa1939b7ee4eef2b0d6

--- cweb/elcurrencyweb/config/database.php
+++ cweb/elcurrencyweb/config/database.php
@@ -82,6 +82,15 @@ $db['elcurrencydb'] = array(
 	'encrypt' => FALSE,	'compress' => FALSE,	'stricton' => FALSE,	'failover' => array(),	'save_queries' => TRUE
 );
 
+// $db['elcurrencydb'] = array(
+// 	'dsn'	=> 'sqlite:'.APPPATH.'/elcurrencydb.db',
+// 	'database' => '',
+// 	'hostname' => '',	'username' => '',	'password' => '',
+// 	'dbdriver' => 'pdo', 	'dbprefix' => '',	'pconnect' => FALSE,	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,	'cachedir' => '',	'char_set' => 'utf8',	'dbcollat' => 'utf8_general_ci',	'swap_pre' => '',
+// 	'encrypt' => FALSE,	'compress' => FALSE,	'stricton' => FALSE,	'failover' => array(),	'save_queries' => TRUE
+// );
+
 $db['default'] = $db['elcurrencydb'];
 // db default is at the end, after definition of "elsistemadb"
 
