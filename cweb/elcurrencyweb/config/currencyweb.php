c5cdbc5e0b9bdf5922ec8af272d1098975f65347
mckaygerhard <mckaygerhard@gmail.com>
2023-05-22 11:58:09 +0000

    fix event id reference after erased table1, only table2 are let

--- cweb/elcurrencyweb/config/currencyweb.php
+++ cweb/elcurrencyweb/config/currencyweb.php
@@ -5,5 +5,5 @@ Currency api integration lib
 
 */
 
-$config['codkey'] = 'SHAR265ql23krjhnou2q34rhi2';//' this is a key that our own clients can use to retrieve secrelty data from this app.. so we can avoid using external paids to others apis;
+$config['codkey'] = 'SHAR265uu23krjhnou2q34rhi2';//' this is a key that our own clients can use to retrieve secrelty data from this app.. so we can avoid using external paids to others apis;
 $config['telegramnews'] = 'https://t.me/s/open_tecnologies';//' https://t.me/s/open_tecnologies the 's' uri section means can be used in a widget.. witout then the phone will load the app ;
